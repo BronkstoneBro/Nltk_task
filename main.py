7146a964c2de093f3f8ef58486e4af7e4b2326a7
BronkstoneBro <atlantwindow@gmail.com>
2024-06-26 10:54:43 +0300

    Ver. 1.0.1 (prod)

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def named_entity_recognition(text: Text):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127,0,0,1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
